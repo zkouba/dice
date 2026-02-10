8466064ad1ea7a447ed71800d26bc36c5235384a
Zdenek Kouba <kouba.zdenek@oracle.com>
2026-02-10 08:52:10 +0100

    advanced terminal app

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,95 @@
-use std::io::Write;
+use std::cmp::{max, min};
+use std::io::{Write};
+use crossterm::{cursor, event, terminal, ExecutableCommand};
+use crossterm::event::{Event, KeyCode, KeyModifiers};
+use crossterm::terminal::ClearType;
 use crate::dice::parser::parse_dice_roll;
 use crate::dice::rolls::{roll, RollResult};
 
 mod dice;
 
 fn main() {
-
+    let prompt = ">: ";
+    let mut stdout = std::io::stdout();
     let mut input = String::new();
-    println!("Welcome to the Dice Roller!");
-    loop {
-        print!(">: ");
-        std::io::stdout().flush().unwrap();
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line"); // CAN PANIC HERE!
-        input = input.trim().to_lowercase();
+    let mut history = Vec::<String>::new();
 
+    println!("\n\nWelcome to the Dice Roller!");
+    stdout.flush().expect("Failed to flush stdout");
+    let mut history_index = 0;
+    loop {
+        print!("{prompt}");
+
+        terminal::enable_raw_mode().expect("Failed to enable raw mode");
+        // Read keys until Enter builds a command line.
+        loop {
+            stdout.flush().expect("Failed to flush stdout");
+            // Blocks until an event is available
+            if let Event::Key(key) = event::read().expect("Failed to read key") {
+                match key.code {
+                    KeyCode::Enter => {
+                        println!();
+                        break;
+                    }
+                    KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                        println!();
+                        return;
+                    }
+                    KeyCode::Esc => {
+                        input.clear();
+                        history_index = 0;
+                        continue;
+                    }
+                    KeyCode::Up => {
+                        if history.is_empty() {
+                            continue;
+                        }
+                        history_index += 1;
+                        history_index = min(history_index, history.len());
+                        input = history[history.len() - history_index].clone();
+
+                        // Redraw prompt + current input.
+                        stdout.execute(cursor::MoveToColumn(0)).unwrap();
+                        stdout.execute(terminal::Clear(ClearType::CurrentLine)).unwrap();
+                        print!("{prompt}{input}");
+                    }
+                    KeyCode::Down => {
+                        if history.is_empty() {
+                            continue;
+                        }
+                        history_index -= 1;
+                        history_index = max(1, history_index);
+                        input = history[history.len() - history_index].clone();
+
+                        // Redraw prompt + current input.
+                        stdout.execute(cursor::MoveToColumn(0)).unwrap();
+                        stdout.execute(terminal::Clear(ClearType::CurrentLine)).unwrap();
+                        print!("{prompt}{input}");
+                    }
+                    KeyCode::Backspace => {
+                        input.pop();
+                        history_index = 0;
+
+                        // Redraw prompt + current input (so removed char is reflected).
+                        stdout.execute(cursor::MoveToColumn(0)).unwrap();
+                        stdout.execute(terminal::Clear(ClearType::CurrentLine)).unwrap();
+                        print!("{prompt}{input}");
+                    }
+                    KeyCode::Char(c) => {
+                        if !key.modifiers.contains(KeyModifiers::CONTROL) {
+                            input.push(c);
+                            history_index = 0;
+                            print!("{c}")
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        stdout.execute(cursor::MoveToColumn(0)).unwrap();
+        terminal::disable_raw_mode().expect("Failed to disable raw mode");
+
+        input = input.trim().to_lowercase();
         if input.is_empty() {
             continue;
         }
@@ -23,8 +97,9 @@ fn main() {
             break;
         }
 
+        history.push(input.clone());
         roll_dice_app(
-            input.split_whitespace().map(|s| s.to_string()).collect()
+            input.clone().split_whitespace().map(|s| s.to_string()).collect()
         );
         input.clear();
     }
@@ -34,7 +109,12 @@ fn roll_dice_app(roll_expressions: Vec<String>) {
     let mut results = Vec::<RollResult>::with_capacity(roll_expressions.len());
     for i in 0..roll_expressions.len() {
         let expression = &roll_expressions[i];
-        let rolls = parse_dice_roll(expression).unwrap();
+        let rolls_res = parse_dice_roll(expression);
+        if rolls_res.is_err() {
+            println!("Error parsing dice roll: {}", roll_expressions[i]);
+            return;
+        }
+        let rolls = rolls_res.unwrap();
         for roll_cmd in rolls {
             results.push(roll(roll_cmd));
         }
