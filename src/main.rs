5cfea4e9658a94e7a20bca351fdad23a7af9d0a4
Zdenek Kouba <kouba.zdenek@oracle.com>
2026-02-10 08:03:07 +0100

    Basic terminal app

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
-use std::env;
+use std::io::Write;
 use crate::dice::parser::parse_dice_roll;
-use crate::dice::rolls::{d12, d6, roll, RollResult};
+use crate::dice::rolls::{roll, RollResult};
 
 mod dice;
 
-// #[derive(Parser, Debug)]
-// struct CliArgs {
-//     #[clap(subcommand)]
-//     command: RollType,
-// }
-//
-// #[derive(Subcommand, Debug)]
-// enum RollType {
-//     D6,
-//     D12,
-//     // #[command(short_flag = 's')]
-//     // Standard{
-//     //     #[arg(long)]
-//     //     d6: u8,
-//     //     #[arg(short, long, action)]
-//     //     favoured: bool,
-//     //     #[arg(short = 'u', long, action)]
-//     //     illfavoured: bool,
-//     //     #[arg(short = 'i', long, action)]
-//     //     inspired: bool,
-//     //     #[arg(short, long, action)]
-//     //     miserable: bool,
-//     //     #[arg(short, long, action)]
-//     //     weary: bool,
-//     //     #[arg(short, long, action)]
-//     //     assisted: bool,
-//     //     #[arg(short='H', long, action)]
-//     //     hope: bool,
-//     //     #[arg(short, long)]
-//     //     bonus: Option<i8>,
-//     // },
-// }
-
 fn main() {
-    let argv: Vec<String> = env::args().collect();
-    roll_dice_app(argv);
+
+    let mut input = String::new();
+    println!("Welcome to the Dice Roller!");
+    loop {
+        print!(">: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line"); // CAN PANIC HERE!
+        input = input.trim().to_lowercase();
+
+        if input.is_empty() {
+            continue;
+        }
+        if input.eq(&"quit".to_string()) || input.eq(&"q".to_string()) || input.eq(&"exit".to_string()) {
+            break;
+        }
+
+        roll_dice_app(
+            input.split_whitespace().map(|s| s.to_string()).collect()
+        );
+        input.clear();
+    }
 }
 
 fn roll_dice_app(roll_expressions: Vec<String>) {
     let mut results = Vec::<RollResult>::with_capacity(roll_expressions.len());
-    for i in 1..roll_expressions.len() {
+    for i in 0..roll_expressions.len() {
         let expression = &roll_expressions[i];
         let rolls = parse_dice_roll(expression).unwrap();
         for roll_cmd in rolls {
@@ -58,45 +46,4 @@ fn roll_dice_app(roll_expressions: Vec<String>) {
         sum += result.value;
     }
     println!("Total: {}", sum);
-    // match roll_command {
-    //     RollType::D6 => {
-    //         let res = d6();
-    //         println!("{}", res);
-    //     }
-    //     RollType::D12 => {
-    //         let res = d12();
-    //         println!("{}", res);
-    //     }
-    //     // RollType::Standard{
-    //     //     d6,
-    //     //     favoured,
-    //     //     illfavoured,
-    //     //     inspired,
-    //     //     miserable,
-    //     //     weary,
-    //     //     assisted,
-    //     //     hope,
-    //     //     bonus
-    //     // } => {
-    //     //     let mut favourableness = if favoured {
-    //     //         Favoured
-    //     //     } else {
-    //     //         Neutral(false)
-    //     //     };
-    //     //     if illfavoured {
-    //     //         favourableness += Illfavoured;
-    //     //     }
-    //     //     let res = roll(
-    //     //         d6,
-    //     //         favourableness,
-    //     //         bonus.or(Some(0)).unwrap(),
-    //     //         hope,
-    //     //         weary,
-    //     //         miserable,
-    //     //         inspired,
-    //     //         assisted,
-    //     //     );
-    //     //     println!("{}", res.to_string());
-    //     // }
-    // }
 }
